608b3617f5553ea5460664a5717a56b768d19f5f
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-15 19:04:19 +0800

    add method Get handler

--- src/api/notes/routes.js
+++ src/api/notes/routes.js
@@ -3,5 +3,10 @@ const routes = handler => [
         method: 'POST',
         path: '/notes',
         handler: handler.postNoteHandler
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: handler.getNotesHandler
     }
 ];
