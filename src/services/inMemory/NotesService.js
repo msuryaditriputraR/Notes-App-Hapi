4312ca64240c3cb9fa711a3ab09cff85dd198953
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-15 18:54:14 +0800

    create editNoteById method

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -43,4 +43,22 @@ class NotesService {
 
         return note;
     }
+
+    editNoteById(id, { title, body, tags }) {
+        const index = this._notes.findIndex(note => note.id === id);
+
+        if (index === -1) {
+            throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+        }
+
+        const updatedAt = new Date().toISOString();
+
+        this._notes[index] = {
+            ...this._notes[index],
+            title,
+            tags,
+            body,
+            updatedAt
+        };
+    }
 }
