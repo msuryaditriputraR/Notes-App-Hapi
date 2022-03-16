8b9bb9febbc71bfd30fcdfc392af270ce3a98b6e
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-16 10:49:04 +0800

    change handle error putNoteById

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -100,11 +100,22 @@ class NotesHandler {
                 message: 'Catatan berhasil diperbarui'
             };
         } catch (error) {
+            if (error instanceof ClientError) {
+                const response = h.response({
+                    status: 'fail',
+                    message: error.message
+                });
+                response.code(error.statusCode);
+                return response;
+            }
+
+            // Server ERROR!
             const response = h.response({
-                status: 'fail',
-                message: error.message
+                status: 'error',
+                message: 'Maaf, terjadi kegagalan pada server kami.'
             });
-            response.code(404);
+            response.code(500);
+            console.error(error);
             return response;
         }
     }
