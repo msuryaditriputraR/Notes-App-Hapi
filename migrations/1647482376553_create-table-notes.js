11a92df062bb0d4b4ec70f140dd9ae87973174ef
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-03-17 10:11:47 +0800

    add pgm up

--- migrations/1647482376553_create-table-notes.js
+++ migrations/1647482376553_create-table-notes.js
@@ -1,5 +1,28 @@
 /* eslint-disable camelcase */
 
-exports.up = pgm => {};
+exports.up = pgm => {
+    pgm.createTable('notes', {
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true
+        },
+        title: {
+            type: 'TEXT',
+            notNull: true
+        },
+        tags: {
+            type: 'TEXT[]',
+            notNull: true
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true
+        }
+    });
+};
 
 exports.down = pgm => {};
