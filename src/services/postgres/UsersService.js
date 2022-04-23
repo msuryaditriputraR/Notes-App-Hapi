2e9be255c76b7ba4d0f4a2255a309109fa62bc36
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-04-23 10:59:57 +0800

    create verifyUserCredential Method

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,8 +1,10 @@
 const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
+
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
     constructor() {
@@ -25,6 +27,28 @@ class UsersService {
             throw new InvariantError('User gagal ditambahkan');
         }
         return result.rows[0].id;
+    }
+
+    async verifyUserCredential(username, password) {
+        const query = {
+            text: 'SELECT id, password FROM users WHERE username = $1',
+            values: [username]
+        };
+
+        const result = await this._pool.query(query);
+        if (!result.rows.length) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+
+        const { id, password: hashedPassword } = result.rows[0];
+
+        const match = await bcrypt.compare(password, hashedPassword);
+
+        if (!match) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+
+        return id;
     }
 
     async getUserById(userId) {
